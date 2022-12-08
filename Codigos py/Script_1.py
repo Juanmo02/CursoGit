b3970386bad0b45e5fcda08c6e2c5750add34108
juanmo02 <juanmo02ucm.es>
2022-12-08 20:05:24 +0100

    Añadir nuevos documentos

---
+++ Codigos py/Script_1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 14 21:54:29 2019
+
+@author: Usuario
+"""
+lista=[1,2,3,5]
+
+i=4
+if i in lista:
+   addf= sum(lista)
+else:
+    addf=("no se puede contar")
+   
